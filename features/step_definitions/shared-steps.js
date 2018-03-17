3523e1ae07a19bb2edf78d4d6acbcdcaaa4d872d
Joe Flintham <joeflintham@gmail.com>
2018-03-17 23:31:16 +0000

    Lint pages

--- features/step_definitions/shared-steps.js
+++ features/step_definitions/shared-steps.js
@@ -1,17 +1,18 @@
 'use strict';
+/* eslint new-cap: 0 */
 
 const chai = require('chai');
 const expect = chai.expect;
 const pages = require('./support/pages');
 
-const { Given, When, Then } = require('cucumber');
+const { Given, Then } = require('cucumber');
 
-Given(/^I am on the '(.*)' page$/, async function (page) {
-    await this.driver.get(pages.getUrl(page));
+Given(/^I am on the '(.*)' page$/, async function(page) {
+	await this.driver.get(pages.getUrl(page));
 });
 
-Then(/^I should see the '(.*)' page$/, async function (page) {
-    const title = await this.driver.getTitle();
-    expect(title).to.equal(pages.getTitle(page));
-});
+Then(/^I should see the '(.*)' page$/, async function(page) {
+	const title = await this.driver.getTitle();
 
+	expect(title).to.equal(pages.getTitle(page));
+});
